cc5a0316599c86536468954b116beef1eba7b7aa
ddalapati <dalapati.debabrata@gmail.com>
2017-04-01 21:08:29 +0530

    Basic functionality added in Project page

--- GPD.WEB/Scripts/custom/GPD.Cntrl.01.ProjectController.js
+++ GPD.WEB/Scripts/custom/GPD.Cntrl.01.ProjectController.js
@@ -2,7 +2,28 @@
 angular.module('Project').controller('ProjectController', function ($scope, $http, $location, CommonServices, ProjectServices) {
     CommonServices.SetDefaultData($scope, $location);
     $scope.data.projects = [];
-    $scope.data.TestData = "Debabrata";
+    $scope.data.sort = {
+        column: 'name',
+        descending: false
+    };
+
+    $scope.OnChangeSorting = function (column) {
+        if ($scope.data.sort.column == column) {
+            $scope.data.sort.descending = !$scope.data.sort.descending;
+        } else {
+            $scope.data.sort.column = column;
+            $scope.data.sort.descending = true;
+        }
+    };
+
+    $scope.ColumnSortClass = function (column) {
+        var retVal = "fa fa-sort";
+        if ($scope.data.sort && $scope.data.sort.column && column == $scope.data.sort.column) {
+            if ($scope.data.sort.descending) { retVal = "fa fa-sort-down"; }
+            else { retVal = "fa fa-sort-up"; }
+        }
+        return retVal;
+    };
 
     var GetAccount = function () {
         return ProjectServices.GetProjects()
