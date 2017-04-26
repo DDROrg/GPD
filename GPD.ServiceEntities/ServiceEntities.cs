135a373c92df7028647c71bd4d56cfc5d37b3334
rprybyslav <rprybyslav@hotmail.com>
2017-04-26 11:58:41 -0400

    adding username + IP

--- GPD.ServiceEntities/ServiceEntities.cs
+++ GPD.ServiceEntities/ServiceEntities.cs
@@ -127,19 +127,25 @@ namespace GPD.ServiceEntities
         [DataMember(Name = "build", Order = 1)]
         public string Build;
 
-        [DataMember(Name = "name", Order = 2)]
+        [DataMember(Name = "client-ip", Order = 2)]
+        public string ClientIP;
+
+        [DataMember(Name = "name", Order = 3)]
         public string Name;
 
-        [DataMember(Name = "plugin-build", Order = 3)]
+        [DataMember(Name = "plugin-build", Order = 4)]
         public string PluginBuild;
 
-        [DataMember(Name = "plugin-source", Order = 4)]
+        [DataMember(Name = "plugin-source", Order = 5)]
         public string PluginSource;
 
-        [DataMember(Name = "type", Order = 5)]
+        [DataMember(Name = "type", Order = 6)]
         public string Type;
 
-        [DataMember(Name = "version", Order = 6)]
+        [DataMember(Name = "username", Order = 6)]
+        public string Username;
+
+        [DataMember(Name = "version", Order = 8)]
         public string Version;
     }   
 
