2b10dea37ea8e34ac022e6cd13e9c98b9d153188
ddalapati <dalapati.debabrata@gmail.com>
2017-06-03 00:55:05 +0530

    Merged

--- GPD.DB/dbo/Tables/gpd_project_user_xref.sql
+++ GPD.DB/dbo/Tables/gpd_project_user_xref.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[gpd_project_user_xref] (
-    [project_id]  UNIQUEIDENTIFIER NULL,
-    [user_id]     INT              NULL,
+    [project_id]  UNIQUEIDENTIFIER NOT NULL,
+    [user_id]     UNIQUEIDENTIFIER NOT NULL,
     [create_date] DATETIME         NOT NULL,
     [update_date] DATETIME         NULL,
     CONSTRAINT [FK_gpd_project_user_xref_gpd_project] FOREIGN KEY ([project_id]) REFERENCES [dbo].[gpd_project] ([project_id]),
