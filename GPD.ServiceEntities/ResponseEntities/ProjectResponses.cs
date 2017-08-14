176918762c3fefa1184a74264d684ed3a6fec5b2
rprybyslav <rprybyslav@hotmail.com>
2017-08-13 22:10:54 -0400

    Update Project

--- GPD.ServiceEntities/ResponseEntities/ProjectResponses.cs
+++ GPD.ServiceEntities/ResponseEntities/ProjectResponses.cs
@@ -1,6 +1,6 @@
 ﻿using System.Runtime.Serialization;
 
-namespace GPD.ServiceEntities.ResponseEntities.AddProject
+namespace GPD.ServiceEntities.ResponseEntities
 {
     [DataContract(Namespace = "http://www.gpd.com", Name = "add-project-response")]
     public class AddProjectResponse
@@ -8,6 +8,28 @@ namespace GPD.ServiceEntities.ResponseEntities.AddProject
         #region Constr
         public AddProjectResponse() : this(false, "-1") { }
         public AddProjectResponse(bool status, string projectId)
+        {
+            this.Status = status;
+            this.Message = string.Empty;
+            this.ProjectId = projectId;
+        }
+        #endregion Constr
+
+        [DataMember(Name = "status", Order = 1)]
+        public bool Status;
+
+        [DataMember(Name = "message", Order = 2)]
+        public string Message;
+
+        [DataMember(Name = "project-id", Order = 3)]
+        public string ProjectId;
+    }
+
+    [DataContract(Namespace = "http://www.gpd.com", Name = "update-project-response")]
+    public class UpdateProjectResponse
+    {
+        #region Constr
+        public UpdateProjectResponse(bool status, string projectId)
         {
             this.Status = status;
             this.Message = string.Empty;
